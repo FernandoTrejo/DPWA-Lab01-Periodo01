2fa3b0f8563d3902b7d42fe2cd8028ea747c22b2
Roberto Funes <robertofunes98@gmail.com>
2021-02-26 19:00:02 -0600

    adding cheges merger

--- Universidades.aspx.cs
+++ Universidades.aspx.cs
@@ -38,15 +38,10 @@ namespace DPWA_Lab01_Periodo01
                     AlmacenDatos almacen = (AlmacenDatos)Session["AlmacenDatos"];
                     Universidad u = almacen.BuscarUniversidad(Int16.Parse(codU));
 
-                txtNombre.Text = u.Nombre;
-                btnAgregarUniversidad.Visible = false;
-                btnEditarUniversidad.Visible = true;
-
-
-            }
                     txtNombre.Text = u.Nombre;
-                    btnAgregarUniversidad.Enabled = false;
-                    btnEditarUniversidad.Enabled = true;
+                    btnAgregarUniversidad.Visible = false;
+                    btnEditarUniversidad.Visible = true;
+
                 }
                 
             }
